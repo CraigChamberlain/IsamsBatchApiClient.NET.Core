bb23184a8e704ee9b454da71688910c15f4f34a5
Craig Chamberlain <craig.chamberlain11@gmail.com>
2023-01-11 14:21:14 +0000

    Add test

---
+++ Isams.BatchApiClient.Core.Tests/ErrorHandling/OAuthError.cs
@@ -0,0 +1,15 @@
+﻿using Isams.BatchApiClient.Core.Services;
+using Xunit;
+
+namespace Isams.BatchApiClient.Core.Tests.ErrorHandling
+{
+    public class OAuthError
+    {
+        [Fact]
+        public void ErrorHandling()
+        {
+            var x = OAuthHttpServices.CreateServices("https://developerdemo.isams.cloud", "IDIDIID", "SECRETSECRET");
+
+        }
+    }
+}
